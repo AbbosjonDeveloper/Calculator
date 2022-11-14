1dc8d665a2f9b10e96797eed27cfb01f920a86ec
Abbos Abdisattarov <abdisattarovabbos36@gmail.com>
2022-11-14 23:26:06 +0500

    if conditions

--- Program.cs
+++ Program.cs
@@ -4,18 +4,19 @@ int firstNumber  = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Type 2nd number: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-// Console.WriteLine($"Adding {firstNumber + secondNumber}");
-// Console.WriteLine($"Subtructing {firstNumber - secondNumber}");
-// Console.WriteLine($"Dividing {firstNumber / secondNumber}");
-// Console.WriteLine($"Multiplying {firstNumber * secondNumber}");
-// Console.WriteLine($"Remaining {firstNumber % secondNumber}");
+if (firstNumber > secondNumber)
+{
+  System.Console.WriteLine("The first number is bigger!");
+}
+else if (secondNumber > firstNumber)
+{
+  System.Console.WriteLine("The second number is bigger!");
+}
+else
+{
+  System.Console.WriteLine("They are equal!");
+}
 
-System.Console.WriteLine($"The 1st number is greater than 2nd number : {firstNumber > secondNumber}");
-System.Console.WriteLine($"The 1st number is greater or equal to  2nd number : {firstNumber >= secondNumber}");
-System.Console.WriteLine($"The 2nd number is greater than 1st number : {firstNumber < secondNumber}");
-System.Console.WriteLine($"The 2nd number is greater or equal to 1st number : {firstNumber <= secondNumber}");
-System.Console.WriteLine($"The 1st number is equal to  2nd number : {firstNumber == secondNumber}");
-System.Console.WriteLine($"The 1st number is not equal to  2nd  number : {firstNumber != secondNumber}");
 
 
 
