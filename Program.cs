28acd5612d5bf76bd6f9fe92dea45b8cd39b462e
Abbos Abdisattarov <abdisattarovabbos36@gmail.com>
2022-11-15 10:57:40 +0500

    Added ternary operators

--- Program.cs
+++ Program.cs
@@ -4,6 +4,20 @@ int firstNumber  = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Type 2nd number: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
+// Ternary operator
+string message = 
+  firstNumber >= 0 
+    ? "1st number is positive"
+    : "1st number is negative";
+
+string message2 = 
+  secondNumber >= 0
+    ? "2nd number is positive"
+    : "2nd number is negative";
+  System.Console.WriteLine(message);
+  System.Console.WriteLine(message2);
+
+
 if (firstNumber > secondNumber)
 {
   System.Console.WriteLine("The first number is bigger!");
