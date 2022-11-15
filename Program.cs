89ab94b246fd8b0eb3b959093013bb028e0df931
Abbos Abdisattarov <abdisattarovabbos36@gmail.com>
2022-11-15 12:21:07 +0500

    Switch case was added to calculator

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 System.Console.WriteLine("Type 1st number: ");
 int firstNumber  = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter oeration (+, -, *, /, % ) :");
+string operation = Console.ReadLine();
 System.Console.WriteLine("Type 2nd number: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
@@ -32,6 +34,26 @@ else
 }
 
 
-
+switch(operation)
+{
+  case "+":
+    System.Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+    break;
+  case "-":
+    System.Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+    break;
+  case "*":
+    System.Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+    break;
+  case "/":
+    System.Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+    break;
+  case "%":
+    System.Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
+    break;
+  default:
+    System.Console.WriteLine("Operation Not Found!");
+    break;
+}
 
 
